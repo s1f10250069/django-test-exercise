cd6d71776c5fe1d4733a90ac572fedc07283fc64
1F10250069 <s1f102500696@iniad.org>
2026-06-25 14:27:19 +0900

    修正

--- todo/views.py
+++ todo/views.py
@@ -18,7 +18,7 @@ def index(request):
     else:
         tasks = Task.objects.order_by('-posted_at')
 
-    context = { 
+    context = {
         'tasks': tasks
     }
     return render(request, 'todo/index.html', context)
